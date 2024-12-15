8d77cfce7ffe0b22c2b5db5ee3436fa05437c5cb
Tomas Ramirez <espelusa01@gmail.com>
2024-12-15 13:41:57 +0100

    Refactor shulker box handling in TerravacuumItem to support all shulker box colors

--- src/main/java/net/aurise/terravacuummod/item/custom/TerravacuumItem.java
+++ src/main/java/net/aurise/terravacuummod/item/custom/TerravacuumItem.java
@@ -1,10 +1,13 @@
 package net.aurise.terravacuummod.item.custom;
 
+import java.util.List;
+
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.StackReference;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -63,15 +66,15 @@ public class TerravacuumItem extends Item{
     public boolean onClicked(ItemStack stack, ItemStack otherStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference) {
 
         // Attach shulker box to the item
-        if (clickType == ClickType.LEFT && otherStack.getItem() == net.minecraft.item.Items.SHULKER_BOX && stack.getComponents().get(DataComponentTypes.CONTAINER) == null) {
+        if (clickType == ClickType.LEFT && isShulkerBox(otherStack.getItem()) && stack.getComponents().get(DataComponentTypes.CONTAINER) == null) {
             stack.set(DataComponentTypes.CONTAINER, otherStack.getComponents().get(DataComponentTypes.CONTAINER));
             otherStack.setCount(0);
             return true;
         }
 
         // Detach shulker box from the item
-        if (clickType == ClickType.RIGHT && otherStack.getItem() == net.minecraft.item.Items.AIR && stack.getComponents().get(DataComponentTypes.CONTAINER) != null) {
-            ItemStack shulker = net.minecraft.item.Items.SHULKER_BOX.getDefaultStack();
+        if (clickType == ClickType.RIGHT && otherStack.getItem() == Items.AIR && stack.getComponents().get(DataComponentTypes.CONTAINER) != null) {
+            ItemStack shulker = Items.SHULKER_BOX.getDefaultStack();
             shulker.set(DataComponentTypes.CONTAINER, stack.getComponents().get(DataComponentTypes.CONTAINER));
             stack.remove(DataComponentTypes.CONTAINER);
             cursorStackReference.set(shulker);
@@ -80,5 +83,14 @@ public class TerravacuumItem extends Item{
 
         return false;
 	}
+
+    // Check if the item is a shulker box (any color)
+    private boolean isShulkerBox(Item item) {
+        List<Item> shulkerBoxes = List.of(Items.SHULKER_BOX, Items.WHITE_SHULKER_BOX, Items.ORANGE_SHULKER_BOX, Items.MAGENTA_SHULKER_BOX, Items.LIGHT_BLUE_SHULKER_BOX, Items.YELLOW_SHULKER_BOX, Items.LIME_SHULKER_BOX, Items.PINK_SHULKER_BOX, Items.GRAY_SHULKER_BOX, Items.LIGHT_GRAY_SHULKER_BOX, Items.CYAN_SHULKER_BOX, Items.PURPLE_SHULKER_BOX, Items.BLUE_SHULKER_BOX, Items.BROWN_SHULKER_BOX, Items.GREEN_SHULKER_BOX, Items.RED_SHULKER_BOX, Items.BLACK_SHULKER_BOX);
+        if (shulkerBoxes.contains(item)) {
+            return true;
+        }
+        return false;
+    }
 
 }
